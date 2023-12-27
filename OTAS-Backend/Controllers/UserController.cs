a7f8077d06f6956de038ff48963e5e4ec8d570b2
Younes <101892216+younes-47@users.noreply.github.com>
2023-12-27 17:51:37 +0100

    New Controller to handle user data

---
+++ OTAS-Backend/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OTAS.DTO.Get;
+using OTAS.Interfaces.IRepository;
+using OTAS.Interfaces.IService;
+using OTAS.Repository;
+
+namespace OTAS.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILdapAuthenticationService _ldapAuthenticationService;
+        private readonly IMapper _mapper;
+        public UserController(IUserRepository userRepository,
+            ILdapAuthenticationService ldapAuthenticationService,
+            IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _ldapAuthenticationService = ldapAuthenticationService;
+            _mapper = mapper;
+        }
+
+        [Authorize(Roles = "requester , decider")]
+        [HttpGet("info")]
+        public UserInfoDTO GetUserInformation()
+        {
+            string username = HttpContext.User.Identity.Name;
+            var userInfo =  _ldapAuthenticationService.GetUserInformation(username);
+            return userInfo;
+        }
+
+
+    }
+}
